8a59e2ce58678c4ef7349011effa0068ee72e75e
Aaron Arney <aarney@leapsparkagency.com>
2021-10-07 12:59:21 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+
+The app is built using TypeScript and Parcel.
+
+
+To run it:
+
+1. Install dependencies.
+   
+    `npm install`
+
+2. Excute the build task, which runs parcel and starts the dev server.
+
+    `npm run-script build`
+
+3. The app will be available at `http://localhost:1234` (port may change if unavailable).
--- src/index.ts
+++ src/index.ts
@@ -9,134 +9,296 @@ import {
     MeshBuilder,
     StandardMaterial,
     Color3,
-    GizmoManager
+    ActionManager,
+    ExecuteCodeAction,
+    AbstractMesh, InterpolateValueAction, SetValueAction,
 } from "@babylonjs/core";
+
+
+type ButtonMap =  Map<string, AbstractMesh>;
+
+
+/**
+ * Simple wrapper class to create solid color materials
+ */
+class SolidColor {
+
+    static make(color: Color3, scene: Scene): StandardMaterial {
+        const mat = new StandardMaterial("white", scene);
+        mat.emissiveColor = color;
+
+        return mat;
+    }
+}
 
 
 class App {
 
+    private readonly engine: Engine;
+    private readonly scene: Scene;
+    private canvas: HTMLCanvasElement;
+    private buttonElements: Set<HTMLButtonElement>;
+
     constructor() {
-        const canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this.createCanvas();
 
-        const engine = new Engine(canvas, true);
-        const scene = new Scene(engine);
+        this.engine = new Engine(this.canvas, true);
+        this.scene = new Scene(this.engine);
 
-        const mWhite = new StandardMaterial("myMaterial", scene);
-        mWhite.emissiveColor = new Color3(1, 1, 1);
-        const mRed = new StandardMaterial("myMaterial", scene);
-        mRed.emissiveColor = new Color3(1, 0, 0);
-        const mGreen = new StandardMaterial("myMaterial", scene);
-        mGreen.emissiveColor = new Color3(0, 1, 0);
-        const mBlue = new StandardMaterial("myMaterial", scene);
-        mBlue.emissiveColor = new Color3(0, 0, 1);
+        this.setupCamera();
+        this.createGeometry();
+        this.createUI();
 
-        const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2,
-            new Vector3(0, 0, 5), scene);
-        camera.attachControl(canvas, true);
-        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        this.loop();
+    }
 
-        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", {diameter: 1}, scene);
-        sphere.position = new Vector3(0, 2, 0);
 
-        // const gizmoManager = new GizmoManager(scene);
-        // gizmoManager.positionGizmoEnabled = true;
-        // gizmoManager.rotationGizmoEnabled = true;
-        // gizmoManager.scaleGizmoEnabled = true;
-        // gizmoManager.boundingBoxGizmoEnabled = true;
-        // gizmoManager.attachableMeshes = [sphere];
-        // gizmoManager.attachToMesh(sphere);
+    /**
+     * The Loop™
+     */
+    private loop() {
+        const actor = this.scene.getMeshByName("actor");
+        const redCube = this.scene.getMeshByName("redCube");
+        const greenCube = this.scene.getMeshByName("greenCube");
+        const blueCube = this.scene.getMeshByName("blueCube");
 
-        const cubeOne: Mesh = MeshBuilder.CreateBox("boxOne", {
-            width: 1,
-            height: 1,
-            depth: 1,
-        }, scene);
-        cubeOne.position = new Vector3(-1.5, 0, 0);
-        cubeOne.material = mRed;
+        const white = SolidColor.make(new Color3(1, 1, 1), this.scene);
+        const red = SolidColor.make(new Color3(1, 0, 0), this.scene);
+        const blue = SolidColor.make(new Color3(0, 1, 0), this.scene);
+        const green = SolidColor.make(new Color3(0, 0, 1), this.scene);
 
-        const cubeTwo: Mesh = MeshBuilder.CreateBox("boxTwo", {
-            width: 1,
-            height: 1,
-            depth: 1,
-        }, scene);
-        cubeTwo.position = Vector3.Zero();
-        cubeTwo.material = mBlue;
+        /**
+         * Not super proud of this implementation. Not only am I writing duplicate code, like the color materials above,
+         * but I'm not keeping logic related to the cubes together. I think ideally I would want to set up an observer
+         * to listen to when an object intersects and then react appropriately there.
+         */
+        this.scene.registerBeforeRender(() => {
+            if (redCube.intersectsMesh(actor)) {
+                redCube.material = white;
+            } else {
+                redCube.material = red;
+            }
 
-        const cubeThree: Mesh = MeshBuilder.CreateBox("boxThree", {
-            width: 1,
-            height: 1,
-            depth: 1,
-        }, scene);
-        cubeThree.position = new Vector3(1.5, 0, 0);
-        cubeThree.material = mGreen;
+            if (greenCube.intersectsMesh(actor)) {
+                greenCube.material = white;
+            } else {
+                greenCube.material = blue;
+            }
 
-        engine.runRenderLoop(() => {
-            scene.render();
-
-            if (sphere.intersectsMesh(cubeOne)) {
-                scene.stopAllAnimations();
-                sphere.material = mWhite;
+            if (blueCube.intersectsMesh(actor)) {
+                blueCube.material = white;
+            } else {
+                blueCube.material = green;
             }
         });
 
-        // scene.registerBeforeRender(function () {
-        //     sphere.lookAt(cubeOne.getAbsolutePosition());
-        // });
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+    }
 
-        const moveTowards = new Animation("movein", "position", 1, Animation.ANIMATIONTYPE_VECTOR3);
+
+    /**
+     *
+     * @private
+     */
+    private createCanvas(): void {
+        this.canvas = document.createElement("canvas");
+        this.canvas.style.width = "100%";
+        this.canvas.style.height = "100%";
+        this.canvas.id = "gameCanvas";
+        document.body.appendChild(this.canvas);
+    }
+
+
+    /**
+     *
+     * @private
+     */
+    private setupCamera(): void {
+        const camera: ArcRotateCamera = new ArcRotateCamera(
+            "Camera",
+            Math.PI / 2,
+            Math.PI / 2,
+            2,
+            new Vector3(0, 0, 5),
+            this.scene
+        );
+        camera.attachControl(this.canvas, true);
+
+        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene);
+    }
+
+
+    /**
+     *
+     * @private
+     */
+    private createGeometry() {
+        const cubeSize = { width: 1, height: 1, depth: 1 };
+
+        const cubeOne: Mesh = MeshBuilder.CreateBox("redCube", cubeSize, this.scene);
+        cubeOne.position = new Vector3(-2.5, 0, 0);
+        cubeOne.material = SolidColor.make(new Color3(1, 0, 0), this.scene);
+
+        const cubeTwo: Mesh = MeshBuilder.CreateBox("blueCube", cubeSize, this.scene);
+        cubeTwo.position = Vector3.Zero();
+        cubeTwo.material = SolidColor.make(new Color3(0, 0, 1), this.scene);
+
+        const cubeThree: Mesh = MeshBuilder.CreateBox("greenCube", cubeSize, this.scene);
+        cubeThree.position = new Vector3(2.5, 0, 0);
+        cubeThree.material = SolidColor.make(new Color3(0, 1, 0), this.scene);
+
+        const sphere: Mesh = MeshBuilder.CreateSphere("actor", {diameter: 1}, this.scene);
+        sphere.position = new Vector3(0, 2, 0);
+        sphere.actionManager = new ActionManager(this.scene);
+
+        /**
+         * This piece really tripped me up. I have no prior experience with Babylon so was trying to figure out how I could
+         * pass an array of meshes to the `paramater` and test against those, instead of having to register separate
+         * actions. I tried passing an array, using `scene.getMeshesByTag` as well as ID but couldn't find something
+         * that worked. In the end since it is only three actions, this works fine. But certainly this isn't scalable.
+         */
+        sphere.actionManager.registerAction(new ExecuteCodeAction({
+                trigger: ActionManager.OnIntersectionEnterTrigger,
+                parameter: cubeOne,
+            }, this.onSphereCollisionCallback.bind(this, cubeOne)
+        ));
+        sphere.actionManager.registerAction(new ExecuteCodeAction({
+                trigger: ActionManager.OnIntersectionEnterTrigger,
+                parameter: cubeTwo,
+            }, this.onSphereCollisionCallback.bind(this, cubeTwo)
+        ));
+        sphere.actionManager.registerAction(
+            new ExecuteCodeAction({
+                trigger: ActionManager.OnIntersectionEnterTrigger,
+                parameter: cubeThree,
+            }, this.onSphereCollisionCallback.bind(this, cubeThree)
+        ));
+    }
+
+    /**
+     *
+     * @private
+     */
+    private onSphereCollisionCallback(collisionObject: AbstractMesh): void {
+        this.scene.stopAllAnimations();
+        collisionObject.material = SolidColor.make(new Color3(1, 1, 1), this.scene);
+        this.enableUI();
+    }
+
+
+    /**
+     *
+     * @private
+     */
+    private enableUI(): void {
+        this.buttonElements.forEach((value: HTMLButtonElement): void => {
+            value.removeAttribute("disabled");
+        });
+    }
+
+
+    /**
+     *
+     * @private
+     */
+    private disableUI(): void {
+        this.buttonElements.forEach((value: HTMLButtonElement): void => {
+            value.setAttribute("disabled", "disabled");
+        });
+    }
+
+
+    /**
+     *
+     * @param name
+     * @param offset
+     * @private
+     */
+    private static createButton(name: string, offset: number): HTMLButtonElement {
+        const button = document.createElement("button");
+        button.style.top = `${offset + 100}px`;
+        button.style.right = "30px";
+        button.textContent = `Move to ${name}`;
+        button.style.width = "100px";
+        button.style.height = "100px";
+        button.style.position = "absolute";
+        button.style.color = "black";
+
+        document.body.appendChild(button);
+
+        return button;
+    }
+
+
+    /**
+     * The event callback for UI buttons.
+     *
+     * @param target
+     * @param key
+     * @private
+     */
+    private onClickCallback(target: AbstractMesh, key: string): void {
+        // Reset actor to its starting paramters
+        const actor = this.scene.getMeshByName("actor");
+        actor.position = new Vector3(0, 2, 0);
+
+        this.disableUI();
+
+        this.createAnimation(actor, target, key);
+    }
+
+
+    /**
+     * I wasn't sure the best way to create a UI with Babylon, as there seems to be a few methods - so I chose the path
+     * of least resistance and used HTML. I've used Phaser a few times for personal projects and the way I've gone about
+     * the UI in the past was by setting up the UI with it's own scene that was loaded on top of the main scene. I imagine
+     * Babylon probably works the same way, but given this is the first time I used the engine I didn't want to spend
+     * a ton of time going about that method.
+     *
+     * @private
+     */
+    private createUI(): void {
+        const buttonMap: ButtonMap = new Map();
+        buttonMap.set("red", this.scene.getMeshByName("redCube"));
+        buttonMap.set("green", this.scene.getMeshByName("greenCube"));
+        buttonMap.set("blue", this.scene.getMeshByName("blueCube"));
+
+        this.buttonElements = new Set();
+
+        let offset: number = 0;
+        buttonMap.forEach((target: AbstractMesh, key: string, _: ButtonMap) => {
+            const button = App.createButton(key, offset);
+            button.addEventListener("click", this.onClickCallback.bind(this, target, key));
+
+            this.buttonElements.add(button);
+
+            offset += 100;
+        });
+    }
+
+
+    /**
+     * Create an opinionated animation on an origin mesh. There is no versatility here, simply moving an object directly
+     * towards a target.
+     *
+     * @param origin
+     * @param target
+     * @param name
+     * @private
+     */
+    private createAnimation(origin: AbstractMesh, target: AbstractMesh, name: string): void {
+        const moveTowards = new Animation(`moveTo${name}`, "position", 1, Animation.ANIMATIONTYPE_VECTOR3);
 
         const keys = [
-            { frame: 0, value: sphere.getAbsolutePosition() },
-            { frame: 10, value: cubeOne.getAbsolutePosition() },
+            { frame: 0, value: origin.getAbsolutePosition() },
+            { frame: 10, value: target.getAbsolutePosition() },
         ];
+
         moveTowards.setKeys(keys);
-        sphere.animations.push(moveTowards);
-
-        const redButton = document.createElement("button");
-        redButton.style.top = "100px";
-        redButton.style.right = "30px";
-        redButton.textContent = "Move to Red";
-        redButton.style.width = "100px"
-        redButton.style.height = "100px"
-        redButton.style.position = "absolute";
-        redButton.style.color = "black";
-        document.body.appendChild(redButton);
-        redButton.addEventListener("click", () => {
-            sphere.lookAt(cubeOne.getAbsolutePosition());
-            scene.beginAnimation(sphere, 0, 1, false);
-        });
-
-        const blueButton = document.createElement("button");
-        blueButton.style.top = "250px";
-        blueButton.style.right = "30px";
-        blueButton.textContent = "Move to Blue";
-        blueButton.style.width = "100px"
-        blueButton.style.height = "100px"
-        blueButton.style.position = "absolute";
-        blueButton.style.color = "black";
-        document.body.appendChild(blueButton);
-        blueButton.addEventListener("click", () => {
-            sphere.lookAt(cubeTwo.getAbsolutePosition());
-            scene.beginAnimation(sphere, 0, 1, false);
-        });
-
-        const greenButton = document.createElement("button");
-        greenButton.style.top = "400px";
-        greenButton.style.right = "30px";
-        greenButton.textContent = "Move to Green";
-        greenButton.style.width = "100px"
-        greenButton.style.height = "100px"
-        greenButton.style.position = "absolute";
-        greenButton.style.color = "black";
-        document.body.appendChild(blueButton);
-        greenButton.addEventListener("click", () => {
-            sphere.lookAt(cubeThree.getAbsolutePosition());
-            scene.beginAnimation(sphere, 0, 1, false);
-        });
+        origin.animations.push(moveTowards);
+        this.scene.beginAnimation(origin, 0, 10, false);
     }
 }
 
